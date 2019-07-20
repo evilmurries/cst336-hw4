b66b7e3ba65e011317578b980ac1044f9036b67d
evilmurries <cpiwarski00@gmail.com>
2019-07-19 22:28:28 -0700

    Working Skeleton

---
+++ app.js
@@ -0,0 +1,33 @@
+// Bring in packages
+const express = require("express");
+const app = express();
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("public"));
+
+const port = 3000;
+
+// Routes
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+app.get("/firms", function(req, res) {
+    res.render("firms.html");
+});
+app.get("/bestpractices", function(req, res) {
+    res.render("bestpractices.html");
+});
+app.get("/types", function(req, res) {
+    res.render("types.html");
+});
+
+//server listener
+app.listen(port, "127.0.0.1", function() {
+    console.log("Express Server is running")
+});
+
+
+/*
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express Server...");
+});
+*/
